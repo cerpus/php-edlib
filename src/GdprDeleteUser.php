41addae09787422dd7f745da34bacf64c963284e
Max Moeschinger <maxmoeschinger@users.noreply.github.com>
2021-12-03 09:03:49 +0100

    Add function to delete gdpr user (#4)

---
+++ src/GdprDeleteUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cerpus\Edlib\ApiClient;
+
+class GdprDeleteUser
+{
+    use DtoTrait;
+
+    private string $requestId;
+    private string $userId;
+    private array $emails;
+
+    public function __construct(string $requestId, string $userId, array $emails = [])
+    {
+        $this->requestId = $requestId;
+        $this->userId = $userId;
+        $this->emails = $emails;
+    }
+}
